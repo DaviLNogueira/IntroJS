71b98113a26c9663be662ade0aa48946056d2c57
Davi Nogueira <davi_nogueira@ufms.br>
2022-05-24 16:02:55 -0400

    Loop de refetição com estruturação do código

--- js/principal.js
+++ js/principal.js
@@ -6,33 +6,39 @@
 var titulo = document.querySelector(".titulo");//uma boa prática é colocar por classes ou ids pq pode mudar o html			
 titulo.textContent = "Aparecida Nutricionista"/*altera a exibição do título*/
 
-var paciente = document.querySelectorAll("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+var pacientes = document.querySelectorAll(".paciente"); // seleciona toda a classe e ide e retorna uma lista 
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+for (var i = 0; i < pacientes.length /*tamanho da lista*/  ; i++){
 
-var tdImc = paciente.querySelector(".info-imc");
+    var paciente = pacientes[i]
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-var pesoEhValido = true;
-var alturaehValido = true; 
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-if (peso < 0 || peso >= 1000){
-    pesoEhValido = false;
-    tdImc.textContent = "Peso inválido"
-    
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = true;
+    var alturaehValido = true; 
+
+    if (peso < 0 || peso >= 1000){
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido"
+        
+    }
+
+
+    if(altura < 0 || altura >= 3) {
+        alturaehValido = false;
+        tdImc.textContent = "Altura inválida"
+    }
+
+    if(alturaehValido && pesoEhValido){
+        var imc =  peso / altura**2;
+        tdImc.textContent = imc.toFixed(2) //fixar apenas duas casas decimais 
 }
 
-
-if(altura < 0 || altura >= 3) {
-    alturaehValido = false;
-    tdImc.textContent = "Altura inválida"
-}
-
-if(alturaehValido && pesoEhValido){
-    var imc =  peso / altura**2;
-    tdImc.textContent = imc
 }
 
 
