9c99baf44d90d482ad514c888c6f4e86b2d108ca
Davi Nogueira <davi_nogueira@ufms.br>
2022-05-25 07:57:10 -0400

    Adiçãa de linhas na tabela através do Js

--- js/principal.js
+++ js/principal.js
@@ -54,5 +54,35 @@ var botaoAdicionar = document.querySelector("#adicionar-paciente");
 
 botaoAdicionar.addEventListener("click",function(){//"escuta alguns tipo de evento,sempre colocar uma ação
     event.preventDefault(); //previne o comportamento padrão
-    console.log("Fui clicado!")
+    var form = document.querySelector("#form-adiciona");
+    var nome = form.nome.value; //seleciona os valores do formulario com os IDs
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr"); //criar um elemento para o css
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome // faz com que os elementos sejam atribuitos ao html
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);// coloca as filhas ou seja as subdivisões dentro da da tag
+    pacienteTr.appendChild(pesoTd); 
+    pacienteTr.appendChild(alturaTd); 
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+    
+     
+    var tabela = document.querySelector("#tabela-pacientes")
+    tabela.appendChild(pacienteTr)
+
+
+
 })
